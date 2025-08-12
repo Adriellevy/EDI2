4acb694f84f1e370de2cd05d7a89473e93f7983b
Adriel <adriellevy2014@gmail.com>
2025-08-12 16:43:14 -0300

    Ejercicio 4 y 5 Terminados

---
+++ TP1(Puertos-timers-contadores)/Ejercicio 4/Pic c compiler/Contador.c
@@ -0,0 +1,17 @@
+#include <Contador.h>
+
+#fuses XT, NOWDT
+#use delay(clock=4000000)  // Frecuencia del cristal (4 MHz en este ejemplo)
+
+void main() {
+   int8 contador = 0;      // Variable para contar de 0 a 255
+
+   set_tris_b(0x00);       // Puerto B como salida (0 = salida, 1 = entrada)
+   output_b(0x00);         // Inicializa el puerto B en 0
+
+   while(TRUE) {
+      output_b(contador);  // Muestra el valor en los LEDs
+      delay_ms(200);       // Espera 200 ms
+      contador++;          // Incrementa el contador (se reinicia automáticamente en 255 -> 0)
+   }
+}
